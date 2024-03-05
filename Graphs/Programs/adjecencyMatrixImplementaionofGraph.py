17b455c2df30493d98e5426db34496f9e471f9bb
Srisai Sudharam <saisudharam123@gmail.com>
2024-03-05 22:36:44 +0530

    hasPath and corrected bfs, dfs

--- Graphs/Programs/adjecencyMatrixImplementaionofGraph.py
+++ Graphs/Programs/adjecencyMatrixImplementaionofGraph.py
@@ -1,3 +1,5 @@
+import queue
+
 class Graph: 
     def __init__(self, noOfVertices):
         self.noOfVertices = noOfVertices
@@ -25,10 +27,69 @@ class Graph:
                 self.__dfsHelper(i, visitedVertices)
         
 
-    def dfs(self):
+    def dfs(self): #dfs corrected
         visitedVertices = [False for i in range(self.noOfVertices) ]
-        self.__dfsHelper(0, visitedVertices)
+        for i in range(self.noOfVertices):
+            if not visitedVertices[i]:
+                self.__dfsHelper(i, visitedVertices)
     
+
+    def __bfsHelper(self, sv, visitedVertices):
+        q = queue.Queue()
+        q.put(sv)
+        visitedVertices[sv] = True
+        while(not q.empty()):
+            vertex = q.get()
+            print(vertex)
+
+            for i in range(self.noOfVertices):
+                if not visitedVertices[i] and self.containsEdge(vertex, i):
+                    q.put(i)
+                    visitedVertices[i] = True # it is better to mark node as visited here there can be duplicate nodes inserted in queue in case of cycle in graph
+
+
+    def bfs(self):
+        visitedVertices = [False for i in range(self.noOfVertices)]
+        for i in range(self.noOfVertices):
+            if not visitedVertices[i]:
+                self.__bfsHelper(i, visitedVertices)
+
+            
+    def __hasPathHelper(self, v1, v2, visitedVertices):
+        visitedVertices[v1] = True
+        if self.adjMatrix[v1][v2] > 0:
+            return True
+        result = False
+        for i in range(self.noOfVertices):
+            if not visitedVertices[i] and self.adjMatrix[v1][i] > 0:
+                result = result or self.__hasPathHelper(i, v2, visitedVertices)
+        return result
+    
+    def has_path(self, v1, v2):
+        visitedVertices = [False for i in range(self.noOfVertices)]
+        return self.__hasPathHelper(v1, v2, visitedVertices)
+
+
+    def hasPathUsingBFS(self, sv, ev):
+        if sv == ev:
+            return True
+        
+        visitedVertices = [False for i in range(self.noOfVertices)]
+        q = queue.Queue()
+        q.put(sv)
+        visitedVertices[sv] = True
+
+        while( not q.empty()):
+            vertex = q.get()
+            
+            for i in range(self.noOfVertices):
+                if self.containsEdge(vertex, i) and not visitedVertices[i]:
+                    if i == ev:
+                        return True
+                    q.put(i)
+                    visitedVertices[i] = True
+        return False
+
     def __str__(self):
         return str(self.adjMatrix)
 
@@ -37,7 +98,13 @@ class Graph:
 
 g = Graph(5)
 g.addEdge(0, 1)
-g.addEdge(1, 3)
-g.addEdge(2, 4)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(3, 4)
 
-g.dfs()
+
+print(g.has_path(0, 2))
+print(g.hasPathUsingBFS(0, 2))
+
+
+g.bfs()
