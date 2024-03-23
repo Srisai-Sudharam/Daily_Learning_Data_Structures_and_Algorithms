81a6a58a5ff7209eee3308771e23d154ce96661f
Srisai Sudharam <saisudharam123@gmail.com>
2024-03-23 22:53:28 +0530

    midpoint in a linked list

---
+++ LinkedList/2 - MidPointLL.py
@@ -0,0 +1,90 @@
+# 1 2 (3) 4 5
+#----------------
+#1 2 (3) 4 5 6 
+
+
+from sys import stdin
+
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+def midPoint(head) :
+    # Write your code here
+    if head is None:
+        return
+    slow = head 
+    fast = head
+    
+    while(fast.next is not None and fast.next.next is not None):
+        slow = slow.next 
+        fast = fast.next.next
+
+    return slow
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+# Main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    
+    head = takeInput()
+    mid = midPoint(head)
+
+    if mid is not None :
+        print(mid.data)
+
+    t -= 1
