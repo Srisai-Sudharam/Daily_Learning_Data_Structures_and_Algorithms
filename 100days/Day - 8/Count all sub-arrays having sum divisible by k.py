96a84000b9ec85912ad8e816da3fdd831fe950e5
Srisai Sudharam <saisudharam123@gmail.com>
2024-04-19 23:07:43 +0530

    count all subarrays

--- 100days/Day - 8/Count all sub-arrays having sum divisible by k.py
+++ 100days/Day - 8/Count all sub-arrays having sum divisible by k.py
@@ -1,10 +1,31 @@
 #Brute Force
+# def subArrayCount(arr, k):
+#     length = len(arr)
+#     count = 0
+#     longest_sub_array = 0
+#     for i in range(length):
+#         for j in range(i+1, length+1):
+#             if sum(arr[i:j]) % k == 0:
+#                 count += 1
+#     return count                 
+
+# optimized   
+
 def subArrayCount(arr, k):
-    length = len(arr)
+    remMap = {}   
+    remMap[0] = 1  
+
     count = 0
-    longest_sub_array = 0
-    for i in range(length):
-        for j in range(i+1, length+1):
-            if sum(arr[i:j]) % k == 0:
-                count += 1
-    return count                    
+    summ = 0
+    for i in range(len(arr)):
+        summ += arr[i]
+
+        rem = summ % k
+        # If rem already in 'remMap'.
+        if(rem in remMap):
+            count += remMap[rem]
+            remMap[rem] += 1
+        else:
+            remMap[rem] = 1
+
+    return count
