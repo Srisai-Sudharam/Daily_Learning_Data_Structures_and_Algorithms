b0309d20a83c159a5dc080de2af229d96cc47d15
Srisai Sudharam <saisudharam123@gmail.com>
2024-04-24 21:17:21 +0530

    max Area brute force

--- 100days/Day -10/Container With Most Water.py
+++ 100days/Day -10/Container With Most Water.py
@@ -1,4 +1,10 @@
+# Brute Force O(N^2), O(1)
 def maxArea(height):
-    
-    # Write your code here.
-    pass
+    length = len(height)
+    maxArea = 0
+    for i in range(length):
+        for j in range(i+1, length):
+            area = min(height[i], height[j]) * (j-i)
+            maxArea = max(area, maxArea)
+    return maxArea
+            
