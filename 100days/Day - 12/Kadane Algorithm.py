03f8b5986ed2aec7fc1d460cb5dbbbe66caa8014
Srisai Sudharam <saisudharam123@gmail.com>
2024-04-29 23:49:50 +0530

    Kadane algorithm still need to be implemented

---
+++ 100days/Day - 12/Kadane Algorithm.py
@@ -0,0 +1,60 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from sys import stdin,setrecursionlimit
+setrecursionlimit(10**7)
+
+def maxSubarraySum(arr, n) :
+
+	# Your code here
+    # return the answer
+	return 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#taking inpit using fast I/O
+def takeInput() :
+	
+    n =  int(input())
+
+    if(n == 0) :
+        return list(), n
+
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+
+    return arr, n
+
+
+#main
+arr, n = takeInput()
+print(maxSubarraySum(arr, n))
