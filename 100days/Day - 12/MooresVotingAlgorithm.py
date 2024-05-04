c7e2c843bc2c859a2039a48cf58fb7140d0c1533
Srisai Sudharam <saisudharam123@gmail.com>
2024-05-04 21:57:56 +0530

    moores vooting algorithm implemented

--- 100days/Day - 12/MooresVotingAlgorithm.py
+++ 100days/Day - 12/MooresVotingAlgorithm.py
@@ -1,3 +1,18 @@
 def findMajorityElement(arr, n):
-	# Write your code here.
-	pass
+	ele = -1
+	count = 0
+	for curr_ele in arr:
+		if count == 0:
+			ele = curr_ele
+			count = 1
+		if ele == curr_ele:
+			count += 1
+		else:
+			count -= 1
+	num = 0
+	for i in arr:
+		if i == ele:
+			num+=1
+	if num > n//2:
+		return ele 
+	return -1
